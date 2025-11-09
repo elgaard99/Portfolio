6eae9d64c9ebd589910921de0a7d601b8aaa0cf0
elgaard99 <sean62359@edu.ucl.dk>
2025-11-09 19:33:33 +0100

    Editor can talk with the k8s db

--- Portfolio/Editor/Program.cs
+++ Portfolio/Editor/Program.cs
@@ -1,9 +1,16 @@
 using Editor.Components;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using SharedLib.Data;
 using SharedLib.Services;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 // Add Bootstrap
 builder.Services.AddBlazorBootstrap();
@@ -11,24 +18,41 @@ builder.Services.AddBlazorBootstrap();
 // Add Logging
 builder.Services.AddLogging();
 
-// Add postgres db
-var pgConnectionString = builder.Configuration.GetConnectionString("Postgres")
-                         ?? throw new NullReferenceException("Postgres");
+// Add redis
+var configuration = builder.Configuration;
+var redisConnString = configuration.GetConnectionString("Redis")
+                      ?? throw new NullReferenceException("Redis connection string not found");
         
-builder.Services.AddDbContextFactory<AppDbContext>(options =>
+var redis = ConnectionMultiplexer.Connect(redisConnString);
+builder.Services.AddDataProtection()
+    .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys")
+    .SetApplicationName("Portfolio");
+        
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = redis.Configuration;
+    options.InstanceName = "Portfolio"; // optional namespace prefix
+});
+        
+// Add postgres
+var pgConnectionString = configuration.GetConnectionString("Postgres") 
+                         ?? throw new NullReferenceException("Postgres connection string not found");
+        
+builder.Services.AddPooledDbContextFactory<AppDbContext>(options =>
     options.UseNpgsql(pgConnectionString));
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
 
 builder.Services.AddScoped<IBlogPostService, BlogPostService>();
 builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+// Add HttpClient
+builder.Services.AddHttpClient();
 
 // Add MinIO
 builder.Services.AddScoped<MinioService>(provider =>
     new MinioService(
         logger: provider.GetRequiredService<ILogger<MinioService>>(),
+        httpClientFactory: provider.GetRequiredService<IHttpClientFactory>(),
+        cache: provider.GetRequiredService<IDistributedCache>(),
         endpoint: builder.Configuration["MinIo:Endpoint"],
         accessKey: builder.Configuration["MinIo:AccessKey"],
         secretKey: builder.Configuration["MinIo:SecretKey"]
